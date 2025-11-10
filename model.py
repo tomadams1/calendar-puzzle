e725e3adf0cf7a79f4002c568c6b1156bdb9eeeb
tomadams <tomadams1999@gmail.com>
2025-11-10 22:32:52 +0000

    Visualised outputs using matplotlib

--- model.py
+++ model.py
@@ -1,14 +1,26 @@
 from ortools.sat.python import cp_model
 from classes import Point, Block
 from transformations import unique_block_transformations
+from matplotlib import pyplot as plt
 
-grid_x = 3
-grid_y = 2
+# grid_x = 3
+# grid_y = 2
+
+# blocks = {
+#     'corner':{(0,0),(0,1),(1,0)},
+#     'line':{(0,0),(0,1)},
+#     'dot':{(0,0)}
+# }
+
+grid_x = 5
+grid_y = 5
 
 blocks = {
-    'corner':{(0,0),(0,1),(1,0)},
-    'line':{(0,0),(0,1)},
-    'dot':{(0,0)}
+    'tee':{(0,0),(0,-1),(0,-2),(-1,0),(1,0)},
+    'chunk':{(0,0),(1,0),(2,0),(1,1),(2,1)},
+    'line':{(0,0),(0,1),(0,2)},
+    'corner':{(0,0),(1,0),(2,0),(3,0),(3,1),(3,2)},
+    'curly':{(0,0),(0,1),(0,2),(1,0),(2,0),(2,1)}
 }
 
 blocks = {name:Block(Point(p) for p in points) for name,points in blocks.items()}
@@ -65,6 +77,8 @@ solver = cp_model.CpSolver()
 solution_printer = cp_model.ObjectiveSolutionPrinter()
 status = solver.solve(model, solution_printer)
 
+output = dict()
+
 for block in blocks:
 
     all_matches = {key:value for key,value in all_blocks.items() if (key[0] == block)}
@@ -73,5 +87,32 @@ for block in blocks:
 
         if solver.boolean_value(value):
 
-            print(f'Place {key[0]} at position ({key[2]},{key[3]})')
-            print(transformations[block][key[1]])
+            # print(f'Place {block} at position ({key[2]},{key[3]})')
+            # print(transformations[block][key[1]])
+            
+            final_points = []
+            for p in transformations[block][key[1]].points:
+
+                new_x = key[2] + p.x
+                new_y = key[3] + p.y
+
+                final_points.append(Point((new_x, new_y)))
+
+            output[block] = Block(final_points)
+
+print(output)
+
+colors = {'tee':'red','chunk':'green','line':'blue','corner':'purple','curly':'cornflowerblue'}
+
+fig, ax = plt.subplots()
+fig.set_size_inches(12,8)
+ax.set_xlim(0, grid_x)
+ax.set_ylim(0, grid_y)
+
+for block_name,block in output.items():
+
+    for point in block.points:
+
+        ax.fill_between([point.x, point.x+1], [point.y, point.y], [point.y+1, point.y+1], color=colors[block_name])
+
+plt.savefig('foo.png')
